c8ab09b7fce363374c28a48f158814ce17777853
Gerrit Farren <gerrit.farre@gmail.com>
2020-12-17 10:11:55 +0100

    fixed bias parameter application for non-renormalized spectra

--- power_spectra/non_linear.py
+++ power_spectra/non_linear.py
@@ -125,6 +125,7 @@ class NonLinearPower(object):
         self.__cosmo = cosmo
         self.__redshift = redshift
         self.__computed = False
+        self.__renormalize = renormalize
 
         if Omfid is None:
             Omfid=cosmo.Omega0_m
@@ -188,6 +189,16 @@ class NonLinearPower(object):
         array_like
             Biased non-linear power spectra (Monopole and Quandrupole depending on value of `ell`)
         """
+
+        #the b bias parameters are actually b_i*norm when the normalization is absorbed into the class-computation
+        if not self.__renormalize:
+            # avoid changing the bias parameters which are passed in in case they are reused
+            bias_params = bias_params.clone()
+
+            bias_params.b1 = bias_params.b1 / self.__cosmo.norm
+            bias_params.b2 = bias_params.b2 / self.__cosmo.norm
+            bias_params.bG2 = bias_params.bG2 / self.__cosmo.norm
+            bias_params.bGamma3 = bias_params.bGamma3 / self.__cosmo.norm
 
         if not self.__computed:
             raise OrderOfOperationsError("Non-linear power spectra can not be obtained. They have not been computed yet.")
@@ -264,9 +275,19 @@ class NonLinearPower(object):
         P2 = self.get_non_linear(2, kbins, bias_params)
         P4 = self.get_non_linear(4, kbins, bias_params)
 
-        P0err = (self.__norm**2. * pk_mult[15] + self.__norm * bias_params.b1 * pk_mult[16] + bias_params.b1**2. * pk_mult[17]) * self.__cosmo.h**3. + bias_params.Pshot
-        P2err = (self.__norm**2. * pk_mult[18] + self.__norm * bias_params.b1 * pk_mult[19]) * self.__cosmo.h**3.
-        P4err = (self.__norm**2. * pk_mult[20]) * self.__cosmo.h**3.
+        # the b bias parameters are actually b_i*norm when the normalization is absorbed into the class-computation
+        if not self.__renormalize:
+            # avoid changing the bias parameters which are passed in in case they are reused
+            bias_params = bias_params.clone()
+
+            bias_params.b1 = bias_params.b1 / self.__cosmo.norm
+            bias_params.b2 = bias_params.b2 / self.__cosmo.norm
+            bias_params.bG2 = bias_params.bG2 / self.__cosmo.norm
+            bias_params.bGamma3 = bias_params.bGamma3 / self.__cosmo.norm
+
+        P0err = (self.__norm**2 * pk_mult[15] + self.__norm * bias_params.b1 * pk_mult[16] + bias_params.b1**2. * pk_mult[17]) * self.__cosmo.h**3. + bias_params.Pshot
+        P2err = (self.__norm**2 * pk_mult[18] + self.__norm * bias_params.b1 * pk_mult[19]) * self.__cosmo.h**3.
+        P4err = (self.__norm**2 * pk_mult[20]) * self.__cosmo.h**3.
 
         covmat = np.zeros((3*k_size, 3*k_size))
         if log_spaced:
